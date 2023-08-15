f5980729e2d23afcf3236c6c928506cd6ece0218
Shane Harris <shane@sidequestvr.com>
2023-08-15 13:20:49 +0200

    added more logs

--- electron/app.ts
+++ electron/app.ts
@@ -161,7 +161,7 @@ class ADB {
                 }
                 cb(tasks);
             } else {
-                ecb(error || 'Nothing to install.' + JSON.stringify(body));
+                ecb(error || 'Nothing to install.' + JSON.stringify(body) + ': ' + token);
             }
         });
     }
